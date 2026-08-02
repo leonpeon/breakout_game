6350129a1a342f0c6c8fd9406b2aceea83f71cfb
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-08-03 07:11:57 +1000

    Set up screen

---
+++ main.py
@@ -0,0 +1,16 @@
+# TO DO:
+# Classes: paddle, ball, bricks, score
+# Paddle: move left and right, detect collision with ball
+# Ball: move diagonally when it hits something
+# Bricks: disappear once the ball touches it
+# Score: +1 for every brick
+
+import turtle
+
+t = turtle.Turtle()
+screen = t.screen
+screen.setup(600, 400)
+
+
+
+screen.exitonclick()
