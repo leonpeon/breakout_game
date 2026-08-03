6318948b4c9005fa57fa7908b07027bce0d275bc
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-08-03 11:43:40 +1000

    Update wall colours

--- bricks.py
+++ bricks.py
@@ -8,7 +8,7 @@ class Brick(Turtle):
         self.shapesize(1, 2)
         self.color("light blue")
         self.start_x = -362
-        self.start_y = 240
+        self.start_y = 220
         self.goto(self.start_x, self.start_y)
 
 class Wall():
@@ -17,9 +17,11 @@ class Wall():
         self.create_wall()
 
     def create_wall(self):
+        colours = ["#F6511D", "#FFB400", "#00A6ED", "#7FB800", "#0D2C54"]
         for n in range(0, 5):
             y_add = n * 25
             for i in range(17):
                 brick = Brick()
+                brick.color(colours[n])
                 brick.goto(brick.start_x + i * 45, brick.start_y - y_add)
                 self.bricks.append(brick)
